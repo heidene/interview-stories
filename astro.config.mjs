8d4d1f8a46a7daf98f41ca3fa9cc25e7383d5e1e
Nico Vandenhove <nico.vandenhove@icloud.com>
2024-08-19 13:44:41 +0200

    Removed Clerk Middleware

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import node from "@astrojs/node";
-import clerk from "@clerk/astro";
 import react from "@astrojs/react";
 
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react(), clerk()],
+  integrations: [mdx(), react()],
   adapter: netlify(),
-  output: "server"
+  output: "server",
 });
