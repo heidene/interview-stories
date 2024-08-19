d4b297556a835fbbae6ea5c029252d9b699eeedc
Nico Vandenhove <nico.vandenhove@icloud.com>
2024-08-19 13:24:29 +0200

    Added Astro Netlify Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import node from "@astrojs/node";
 import clerk from "@clerk/astro";
-
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), react(), clerk()],
-  adapter: node({ mode: "standalone" }),
-  output: "server",
+  adapter: netlify(),
+  output: "server"
 });
